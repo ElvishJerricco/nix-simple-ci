b8810b0fe36bdccd565fd67d9eba1a2611fb064a
David Johnson <david@Galoiss-MacBook-Pro.local>
2017-07-01 08:31:31 -0700

    Added command line options

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE OverloadedStrings #-}
@@ -16,7 +18,6 @@ import Data.Aeson
 import Data.Aeson.Lens
 import Data.Aeson.TH
 import Data.ByteString (ByteString)
-import qualified Data.ByteString.Char8 as BS
 import Data.Char (toLower)
 import Data.Foldable (for_)
 import Data.List (stripPrefix)
@@ -26,10 +27,18 @@ import qualified Data.Text as T
 import Network.HTTP.Client hiding (Proxy)
 import Network.HTTP.Client.TLS
 import Network.Wai.Handler.Warp
+import Options.Generic
 import Servant
 import Servant.GitHub.Webhook
-import System.Environment
 import Turtle hiding (stripPrefix)
+
+data Option = Option
+  { secret :: ByteString
+  , oauth :: ByteString
+  , port :: Maybe Int
+  } deriving (Show, Eq, Generic)
+
+instance ParseRecord Option
 
 data BuildCommit = BuildCommit
   { _buildCommit_repo :: Text
@@ -62,13 +71,14 @@ type API
 
 main :: IO ()
 main = do
-  [secret, oauth] <- getArgs
-  mgr             <- newTlsManager
-  sem             <- newQSem 1
-  putStrLn "Starting server on 8080"
-  run 8080 $ serveWithContext (Proxy @API)
-                              (gitHubKey (pure $ BS.pack secret) :. EmptyContext)
-                              (server mgr (BS.pack oauth) sem)
+  Option {..} <- getRecord "nix-simple-ci"
+  mgr         <- newTlsManager
+  sem         <- newQSem 1
+  let port' = fromMaybe 8080 port
+  putStrLn $ "Starting server on " ++ show port'
+  run port' $ serveWithContext (Proxy @API)
+                               (gitHubKey (pure secret) :. EmptyContext)
+                               (server mgr oauth sem)
 
 server :: Manager -> ByteString -> QSem -> Server API
 server mgr oauth sem WebhookPushEvent ((), obj) = liftIO $ do
